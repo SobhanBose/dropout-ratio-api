659931064108adf4aceb595d1542e092538ffe53
Sobhan Bose <bosesobhan2003@gmail.com>
2024-04-03 20:11:51 +0530

    corrected paths

--- v1/getData/router.py
+++ v1/getData/router.py
@@ -6,7 +6,7 @@ from v1.utils.read_data import get_df, TARGET_COLS
 router = APIRouter(prefix="/getData")
 
 
-df = get_df("1e5ENZNsD10pWJ0ZxzLngf1N5eh2CfkoT", "data\\hackathon_data")
+df = get_df("1e5ENZNsD10pWJ0ZxzLngf1N5eh2CfkoT", "data/hackathon_data")
 
 
 @router.get("/byRegion")

5dea08a5103c5a2f10bf22aa5f55120cec94bb83
Sobhan Bose <bosesobhan2003@gmail.com>
2024-04-04 10:24:14 +0530

    added a formatted data get route

--- v1/getData/router.py
+++ v1/getData/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 import json
-from v1.utils.read_data import get_df, TARGET_COLS
+from v1.utils.read_data import get_df, TARGET_COLS, NAME_MAPPING
 
 
 router = APIRouter(prefix="/getData")
@@ -20,6 +20,16 @@ def get_data_by_criterion(criterion: str) -> dict:
     data = {}
     for col in TARGET_COLS:
         data[col] = {"region": df.loc[:, "region"].to_list(), criterion: df.loc[:, criterion].to_list(), col: df.loc[:, col].to_list()}
+    
+    return data
+
+
+@router.get("/byCriterionFormatted")
+def get_data_by_criterion_formatted(criterion: str) -> dict:
+    data = {}
+    for col in TARGET_COLS:
+        data["title"] = NAME_MAPPING[criterion]
+        data[col] = {"label": f'{NAME_MAPPING[criterion]} vs {NAME_MAPPING[col]}', "region": df["region"].to_list(), "data": [{"x": x, "y": y} for x, y in zip(df[col].to_list(), df[criterion].to_list())]}
     
     return data
     
